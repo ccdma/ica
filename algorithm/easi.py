99a43b41906e69a0ce1b99b257517d918faaf276
face0u0 <me@face0u0.net>
2021-09-16 13:04:49 +0900

    add easi

--- algorithm/easi.py
+++ algorithm/easi.py
@@ -5,10 +5,10 @@ from scipy.special import eval_chebyt
 
 np.set_printoptions(precision=4)
 np.set_printoptions(suppress=True)
-np.random.seed(seed=1)
+np.random.seed(seed=2)
 
-SAMPLE = 2
-SERIES = 1000
+SAMPLE = 4
+SERIES = 10000
 
 # チェビシェフ系列 
 # a0: 初期値
@@ -35,17 +35,19 @@ def g(x):
     return -np.tanh(x)
 MU = 0.001953125
 
+YT = []
 for x in X.T:
     y = np.array([B @ x]).T
+    YT.append(y[:,0])
     V = y @ y.T - np.eye(SAMPLE) + g(y) @ y.T - y @ g(y).T
     B = B - MU * V @ B
-Y = B @ X
+Y = np.array(YT).T
 
 # ica = FastICA(n_components=SERIES, random_state=0)
 # _Y = ica.fit_transform(X.T).T * 8
 
 data = [
-    [S,"source"],
+    [S, "source"],
     [X, "mixed"],
     [Y, "reconstruct"]
 ]
@@ -55,6 +57,7 @@ for i in range(len(data)):
     param = data[i][0]
     title = data[i][1]
     ax[i].set_title(title)
+    ax[i].set_xlim(SERIES-500,SERIES)
     for j in range(param.shape[0]):
         ax[i].plot(param[j, :])
 fig.tight_layout()
