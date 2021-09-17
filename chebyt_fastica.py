3e2a224780a3639b742f678fd0baa6d6249123c6
face0u0 <me@face0u0.net>
2021-09-17 20:24:08 +0900

    chebyt return map

---
+++ chebyt_fastica.py
@@ -0,0 +1,39 @@
+from seed.mixture import mixed_matrix
+from seed.chebyt import chebyt_series
+from algorithm.fastica import fast_ica
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.set_printoptions(precision=4)
+np.set_printoptions(suppress=True)
+np.random.seed(seed=2)
+
+SAMPLE = 4
+SERIES = 1000
+
+# 完全にランダムな混合
+A = mixed_matrix(SAMPLE)
+
+# 元信号
+S = np.array([chebyt_series(i+2, np.random.rand()/2, SERIES) for i in range(SAMPLE)])
+
+# 混合された信号
+X = A @ S # [[x1(0), x1(0.1), x1(0.2)],[x2(0), x2(0.1), x2(0.2)]]のような感じ、右に行くにつれて時間が経過する
+
+result = fast_ica(X, _assert=False)
+Y = result.Y
+
+data = [
+    [S, "source"],
+    [X, "mixed"],
+    [Y, "reconstruct"]
+]
+
+fig, ax = plt.subplots(1, len(data))
+for i in range(len(data)):
+    ax[i].set_title(data[i][1])
+    ax[i].scatter(data[i][0][0][0:-1], data[i][0][0][1:], s=10, c="blue", alpha=0.5)
+    ax[i].scatter(data[i][0][1][0:-1], data[i][0][1][1:], s=10, c="green", alpha=0.5)
+    ax[i].scatter(data[i][0][2][0:-1], data[i][0][2][1:], s=10, c="red", alpha=0.5)
+fig.tight_layout()
+plt.show()
--- seed/mixture.py
+++ seed/mixture.py
@@ -4,5 +4,5 @@ import numpy as np
 -0.5~+0.5なる混合行列を作成
 size: 正方行列のサイズ
 """
-def mixed_matrix(size: int):
+def mixed_matrix(size: int) -> np.ndarray:
     return np.array([[np.random.rand()-0.5 for i in range(size)] for j in range(size) ])
