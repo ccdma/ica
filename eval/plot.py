92fd4d82492613d687c7e2f1c44a939a8f70f674
face0u0 <me@face0u0.net>
2021-09-26 12:56:18 +0900

    push

--- eval/plot.py
+++ eval/plot.py
@@ -1,6 +1,12 @@
 import dataclasses as d
 import matplotlib.pyplot as plt
 import numpy as np
+from typing import List 
+
+@d.dataclass
+class EachOption:
+    label: str = None
+    color: str = None
 
 @d.dataclass
 class ReturnmapOption:
@@ -8,11 +14,20 @@ class ReturnmapOption:
     A: np.ndarray
     title: str = None
     start_index: int = 0
+    each: List[EachOption] = None
+
+    def __post_init__(self):
+        if self.each == None:
+            self.each = []
+        for _ in range(self.A.shape[0]-len(self.each)):
+            self.each.append(EachOption())
 
     def plot(self, ax: plt.Axes):
         ax.set_title(self.title)
         for j in range(self.A.shape[0]): # 各系列
-            ax.scatter(self.A[j][self.start_index:-1], self.A[j][self.start_index+1:], s=10, alpha=0.5)
+            ax.scatter(self.A[j][self.start_index:-1], self.A[j][self.start_index+1:], s=10, alpha=0.5, label=self.each[j].label)
+        if any(map(lambda e : e.label != None, self.each)):
+            ax.legend()
 
 @d.dataclass
 class PlotOption:
@@ -21,12 +36,19 @@ class PlotOption:
     title: str = None
     start_index: int = 0
     end_index: int = None
+    each: List[EachOption] = None
 
     def __post_init__(self):
+        if self.each == None:
+            self.each = []
+        for _ in range(self.A.shape[0]-len(self.each)):
+            self.each.append(EachOption())
         if self.end_index == None:
             self.end_index = self.A.shape[1]
 
     def plot(self, ax: plt.Axes):
         ax.set_title(self.title)
         for j in range(self.A.shape[0]):
-            ax.plot(self.A[j, self.start_index:self.end_index], alpha=0.7)
+            ax.plot(self.A[j, self.start_index:self.end_index], alpha=0.7, label=self.each[j].label)
+        if any(map(lambda e : e.label != None, self.each)):
+            ax.legend()
--- fastica_test.py
+++ fastica_test.py
@@ -1,4 +1,4 @@
-from eval.plot import PlotOption, ReturnmapOption
+from eval.plot import EachOption, PlotOption, ReturnmapOption
 from algorithm.fastica import fast_ica
 from eval.product import inner_matrix
 from eval.seed import chebyt_series, concat, mixed_matrix
@@ -10,47 +10,59 @@ np.set_printoptions(precision=4)
 np.set_printoptions(suppress=True)
 np.random.seed(seed=1)
 
-SAMPLE = 4
+
 SERIES = 1000
 
-# 完全にランダムな混合
-A = mixed_matrix(SAMPLE)
+def main():
+    S = concat(
+        [chebyt_series(i+2, np.random.rand()*2-1, SERIES) for i in range(2)],
+        [[np.sin(j/10/np.sqrt(i+1)) for j in range(SERIES)] for i in range(2)]
+    )
+    desc = [
+        EachOption(label="T2"),
+        EachOption(label="T3")
+    ]
+    title = "sin_chebyt"
 
-# 元信号
-S = concat(
-    [chebyt_series(i+2, np.random.rand()*2-1, SERIES) for i in range(2)],
-    [[np.sin(j/10/np.sqrt(i+1)) for j in range(SERIES)] for i in range(SAMPLE-2)]
-)
+    SAMPLE = S.shape[0]
 
-# 混合された信号
-X = A @ S # [[x1(0), x1(0.1), x1(0.2)],[x2(0), x2(0.1), x2(0.2)]]のような感じ、右に行くにつれて時間が経過する
+    # 完全にランダムな混合
+    A = mixed_matrix(SAMPLE)
 
-Y = fast_ica(X, _assert=False).Y
+    # 混合された信号
+    X = A @ S # [[x1(0), x1(0.1), x1(0.2)],[x2(0), x2(0.1), x2(0.2)]]のような感じ、右に行くにつれて時間が経過する
 
-pprint.pprint(inner_matrix(S))
-pprint.pprint(inner_matrix(X))
-pprint.pprint(inner_matrix(Y))
+    Y = fast_ica(X, _assert=False).Y
 
-pltOps = [
-    PlotOption(S, title="source", end_index=100),
-    PlotOption(X, title="mixed", end_index=100),
-    PlotOption(Y, title="reconstruct", end_index=100)
-]
-# グラフの作成
-fig, ax = plt.subplots(len(pltOps), 1)
-for i in range(len(pltOps)):
-    pltOps[i].plot(ax[i])
-fig.tight_layout()
+    pprint.pprint(inner_matrix(S))
+    pprint.pprint(inner_matrix(X))
+    pprint.pprint(inner_matrix(Y))
 
-retOps = [
-    ReturnmapOption(S, title="source"),
-    ReturnmapOption(X, title="mixed"),
-    ReturnmapOption(Y, title="recnstruct"),
-]
-# リターンマップの作成
-fig, ax = plt.subplots(1, len(retOps))
-for i in range(len(retOps)):
-    retOps[i].plot(ax[i])
-fig.tight_layout()
+    pltOps = [
+        PlotOption(S, title="source", end_index=100, each=desc),
+        PlotOption(X, title="mixed", end_index=100),
+        PlotOption(Y, title="reconstruct", end_index=100)
+    ]
+    # グラフの作成
+    fig, ax = plt.subplots(len(pltOps), 1)
+    for i in range(len(pltOps)):
+        pltOps[i].plot(ax[i])
+    fig.tight_layout()
+    fig.suptitle(f"{title}_plot")
+    fig.savefig(f"{title}_plot.png")
 
-plt.show()
+    retOps = [
+        ReturnmapOption(S, title="source"),
+        ReturnmapOption(X, title="mixed"),
+        ReturnmapOption(Y, title="recnstruct"),
+    ]
+    # リターンマップの作成
+    fig, ax = plt.subplots(1, len(retOps))
+    for i in range(len(retOps)):
+        retOps[i].plot(ax[i])
+    fig.tight_layout()
+    fig.suptitle(f"{title}_returnmap")
+    fig.savefig(f"{title}_returnmap.png")
+
+if __name__ == '__main__':
+    main()
