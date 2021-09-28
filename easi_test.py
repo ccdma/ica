31b960a7ca8b8fd72389b03e5f4f54af93abcad1
face0u0 <me@face0u0.net>
2021-09-28 14:28:04 +0900

    add data

--- easi_test.py
+++ easi_test.py
@@ -1,62 +1,98 @@
-from eval.plot import PlotOption, ReturnmapOption
 from algorithm.easi import EASI
+from eval.dataset import DATADICT, Dataset
+from eval.report import Printer
+from eval.plot import PlotOption, ReturnmapOption
 from eval.product import inner_matrix
-from eval.seed import chebyt_series, concat, mixed_matrix
+from eval.seed import mixed_matrix
 import numpy as np
 import matplotlib.pyplot as plt
-import pprint 
+import sys
 
 np.set_printoptions(precision=4)
 np.set_printoptions(suppress=True)
-np.random.seed(seed=1)
+np.random.seed(seed=4)
 
-SAMPLE = 4
-SERIES = 50000
+def easi_report(dataset: Dataset):
 
-# 完全にランダムな混合
-A = mixed_matrix(SAMPLE)
+    S = dataset.S
+    labels = dataset.labels
+    key = dataset.key
 
-# 元信号
-S = concat(
-    [chebyt_series(i+2, np.random.rand()*2-1, SERIES) for i in range(2)],
-    [[np.sin(j/10/np.sqrt(i+1)) for j in range(SERIES)] for i in range(SAMPLE-2)]
-)
+    f = open(f"out/easi/{key}.txt", 'w+')
+    p = Printer(f, sys.__stdout__)
 
-# 混合された信号
-X = A @ S # [[x1(0), x1(0.1), x1(0.2)],[x2(0), x2(0.1), x2(0.2)]]のような感じ、右に行くにつれて時間が経過する
+    SERIES = S.shape[1]
+    SAMPLE = S.shape[0]
 
-easi = EASI(SAMPLE)
+    p.print(f"SERIES={S.shape[1]}")
 
-YT = []
-for x in X.T:
-    y = easi.update(x)
-    YT.append(y)
-Y = np.array(YT).T
+    # 完全にランダムな混合
+    A = mixed_matrix(SAMPLE)
 
-pprint.pprint(inner_matrix(S))
-pprint.pprint(inner_matrix(X))
-pprint.pprint(inner_matrix(Y))
+    # 混合された信号
+    X = A @ S # [[x1(0), x1(0.1), x1(0.2)],[x2(0), x2(0.1), x2(0.2)]]のような感じ、右に行くにつれて時間が経過する
 
-pltOps = [
-    PlotOption(S, title="source", start_index=SERIES-min(500, SERIES)),
-    PlotOption(X, title="mixed", start_index=SERIES-min(500, SERIES)),
-    PlotOption(Y, title="reconstruct", start_index=SERIES-min(500, SERIES))
-]
-# グラフの作成
-fig, ax = plt.subplots(len(pltOps), 1)
-for i in range(len(pltOps)):
-    pltOps[i].plot(ax[i])
-fig.tight_layout()
+    easi = EASI(SAMPLE)
 
-retOps = [
-    ReturnmapOption(S, title="source", start_index=SERIES-min(500, SERIES)),
-    ReturnmapOption(X, title="mixed", start_index=SERIES-min(500, SERIES)),
-    ReturnmapOption(Y, title="recnstruct", start_index=SERIES-min(500, SERIES)),
-]
-# リターンマップの作成
-fig, ax = plt.subplots(1, len(retOps))
-for i in range(len(retOps)):
-    retOps[i].plot(ax[i])
-fig.tight_layout()
+    YT = []
+    for x in X.T:
+        y = easi.update(x)
+        YT.append(y)
+    Y = np.array(YT).T
 
-plt.show()
+    p.print("A:")
+    p.pprint(A)
+
+    p.print("inner(S):")
+    p.pprint(inner_matrix(S))
+    p.print("inner(X):")
+    p.pprint(inner_matrix(X))
+    p.print("inner(Y):")
+    p.pprint(inner_matrix(Y))
+
+    start_index = SERIES-min(500, SERIES)
+
+    pltOps = [
+        PlotOption(S, title="source", labels=labels, start_index=start_index),
+        PlotOption(X, title="mixed", start_index=start_index),
+        PlotOption(Y, title="reconstruct", start_index=start_index)
+    ]
+    # グラフの作成
+    fig, ax = plt.subplots(len(pltOps), 1)
+    for i in range(len(pltOps)):
+        pltOps[i].plot(ax[i])
+    fig.tight_layout()
+    fig.suptitle(f"{key}", x=0.1, y=0.97)
+    fig.set_figheight(5)
+    fig.set_figwidth(10)
+    fig.savefig(f"out/easi/{key}_p.png")
+
+    retOps = [
+        ReturnmapOption(S, title="source", labels=labels, start_index=start_index),
+        ReturnmapOption(X, title="mixed", start_index=start_index),
+        ReturnmapOption(Y, title="recnstruct", start_index=start_index),
+    ]
+    # リターンマップの作成
+    fig, ax = plt.subplots(1, len(retOps))
+    for i in range(len(retOps)):
+        retOps[i].plot(ax[i])
+    fig.tight_layout()
+    fig.suptitle(f"{key}", x=0.1, y=0.97)
+    fig.set_figheight(5)
+    fig.set_figwidth(12)
+    fig.savefig(f"out/easi/{key}_r.png")
+
+    f.close()
+
+if __name__ == '__main__':
+    easi_report(DATADICT["isin_1000"])
+    easi_report(DATADICT["sqrtsin_1000"])
+    easi_report(DATADICT["sin2_chebyt2_1000"])
+    easi_report(DATADICT["chebyt2-5_1000"])
+    easi_report(DATADICT["chebyt10_1000"])
+    easi_report(DATADICT["sin+x_1000"])
+    easi_report(DATADICT["sin+2x_1000"])
+    easi_report(DATADICT["A*sin_1000"])
+    easi_report(DATADICT["Adiv2*sin_1000"])    
+    easi_report(DATADICT["Adiv2*sin_5000"])    
+    easi_report(DATADICT["A*sqrtsin_1000"])
