972c81ae1b8bad6f4ed1bf751ffc620e8f92ffcc
face0u0 <me@face0u0.net>
2021-10-08 11:48:03 +0900

    rm unused code

--- weyl.py
+++ weyl.py
@@ -14,22 +14,4 @@ p = Printer.with_sysout()
 
 S = np.array([weyl_series(np.random.rand(), np.random.rand(), 1000) for _ in range(4)])
 
-N = int(S.shape[1]/2)
-
-def C(i, k, l, S=S):
-    if abs(l) >= N:
-        return 0
-    elif l >= 0:
-        _sum = 0
-        for n in range(N-1):
-            _sum += np.conjugate(S[i, n+l]) * S[k, n]
-        return _sum
-    else:
-        _sum = 0
-        for n in range(N+1):
-            _sum += np.conjugate(S[i, n]) * S[k, n-l]
-        return _sum
-
-theta = lambda i, k, l : C(i, k, l) + C(i, k, l-N)
 p.pprint(theta_matrix(S, 2))
-p.pprint(theta(3, 3, 2))
